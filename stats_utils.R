36e562d061cdfa46ace71d2c4000ff3641ff71a4
gabnasello <gabriele.nasello@gmail.com>
2023-10-16 18:57:21 -0400

    remove rename mean column

--- stats_utils.R
+++ stats_utils.R
@@ -8,14 +8,19 @@
   #to be summariezed
 # groupnames : vector of column names to be used as
   # grouping variables
+
 data_summary <- function(data, varname, groupnames){
+    
   require(plyr)
   summary_func <- function(x, col){
-    c(mean = mean(x[[col]], na.rm=TRUE),
-      sd = sd(x[[col]], na.rm=TRUE))
-  }
+                                   c(mean = mean(x[[col]], na.rm=TRUE),
+                                   sd = sd(x[[col]], na.rm=TRUE))
+                                   }
+  
   data_sum<-ddply(data, groupnames, .fun=summary_func,
                   varname)
-  data_sum <- rename(data_sum, c("mean" = varname))
+  #data_sum <- rename(data_sum, c("mean" = varname))
+    
  return(data_sum)
+    
 }
